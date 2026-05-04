869c3c3c8542c82927e3615de4258e22eb801a97
Da4Rkclown <118552956+Da4Rkclown@users.noreply.github.com>
2026-05-04 10:22:00 +0530

    added status code

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -105,9 +105,10 @@ export const signIn=async (req:Request,res:Response)=>{
         }
 
    } catch (error) {
-        console.log("signIn api err");
-        console.log(error);
-        return 
+        res.status(400).json({
+                msg:"sign in api error"
+            })
+            return
     }
 }
 
