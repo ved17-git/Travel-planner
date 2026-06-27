9f5b8a9e3c3d21df791ff82925723e0060f73e5f
Da4Rkclown <118552956+Da4Rkclown@users.noreply.github.com>
2026-06-27 11:06:59 +0530

    create trip limiter

--- backend/src/rateLimitter.ts
+++ backend/src/rateLimitter.ts
@@ -2,7 +2,7 @@ import { rateLimit } from 'express-rate-limit'
 
 export const createTripLimitter = rateLimit({
 	windowMs: 120 * 60 * 1000, // 2 hr
-	limit: 10, // it will be 8req/2hr 
+	limit: 15 // it will be 8req/2hr 
 	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
@@ -12,7 +12,7 @@ export const createTripLimitter = rateLimit({
 
 export const updateTripLimitter = rateLimit({
 	windowMs: 120 * 60 * 1000, // 2 hr
-	limit: 15, // it will be 8req/2hr 
+	limit: 15, // it will be 15req/2hr 
 	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
